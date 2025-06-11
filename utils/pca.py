cb424690bfdb9d7f639e014c0d3ef006a150f2cd
Felix Janda <felix.janda@posteo.de>
2025-06-11 15:34:20 -0400

    remove old version of pca

--- utils/pca.py
+++ utils/pca.py
@@ -3,11 +3,6 @@ import torch
 def pca(x, dims=2):
     A = x.transpose(0,1)
 
-    if True:
-        torch.manual_seed(1)
-        V = torch.pca_lowrank(A)[2]
-        return torch.matmul(A, V[:, :dims])
-    else:
-        A = A - A.mean(dim=0)
-        U, S, _ = torch.linalg.svd(A)
-        return (U@torch.diag(S))[:, :dims]
+    A = A - A.mean(dim=0)
+    U, S, _ = torch.linalg.svd(A)
+    return (U@torch.diag(S))[:, :dims]
