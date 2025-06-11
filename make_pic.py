94373fded77339a5abf4590691e6f911bc630de2
Felix Janda <felix.janda@posteo.de>
2025-06-11 15:54:45 -0400

    clean make_pic.py

--- make_pic.py
+++ make_pic.py
@@ -1,3 +1,5 @@
+import argparse
+
 import torch
 
 from utils.checkpoints import *
@@ -7,34 +9,45 @@ from utils.pca import *
 
 import matplotlib.pyplot as plt
 
-filename = 'MyFigure.png'
 
-task_dir = "tasks/first"
+parser = argparse.ArgumentParser(prog='make_pic.py', description='Create a figure')
+parser.add_argument('task', help="task directory")
+parser.add_argument('-f', '--file', default='MyFigure.png')
+parser.add_argument('-d', '--dims', type=int, default=2, help="number of PCA dimensions")
 
-_, _, layers, _, n, _, _, num_epochs = load_cfg(task_dir)
+args = parser.parse_args()
+
+assert args.dims in (2, 3)
+
+
+_, _, layers, _, n, _, _, num_epochs = load_cfg(args.task)
+
 
 model = MyModel(n, layers['embed_dim'], layers['hidden_dim'])
+load_checkpoint(model, None, args.task, final=True)
 
-load_checkpoint(model, None, task_dir, final=True)
 
-M = pca(model.embed1.weight)
+M = pca(model.embed1.weight, dims=3).detach().numpy()
 
-X = []
-Y = []
-S = []
-m = []
-for i in range(n):
-    X.append(float(M[i][0]))
-    Y.append(float(M[i][1]))
-
-# plot
+X = M[:,0]
+Y = M[:,1]
+Z = M[:,2]
 
 fig = plt.figure()
-ax = fig.add_subplot()
+if args.dims == 2:
+    ax = fig.add_subplot()
 
-ax.scatter(X, Y)
+    ax.scatter(X, Y)
 
-for i in range(n):
-    ax.text(X[i], Y[i], str(i))
+    for i in range(n):
+        ax.text(X[i], Y[i], str(i))
+else:
+    ax = fig.add_subplot(projection='3d')
 
-fig.savefig(filename, dpi=200)
+    ax.scatter(X, Y, Z)
+
+    for i in range(n):
+        ax.text(X[i], Y[i], Z[i], str(i))
+
+fig.savefig(args.file, dpi=200)
+
