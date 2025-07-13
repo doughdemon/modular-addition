4326769598baa93bf70cc69443e344b2c06fae23
Felix Janda <felix.janda@posteo.de>
2025-07-13 07:49:06 -0400

    pca: add comments

--- utils/pca.py
+++ utils/pca.py
@@ -1,8 +1,9 @@
 import torch
 
 def pca(x, dims=2):
-    A = x.transpose(0,1)
+    # x: (N, D)
+    A = x.transpose(0,1) # (D, N)
 
-    A = A - A.mean(dim=0)
-    U, S, _ = torch.linalg.svd(A)
-    return (U@torch.diag(S))[:, :dims], S[:dims]
+    A = A - A.mean(dim=0) # (D, N)
+    U, S, _ = torch.linalg.svd(A) # (D, D) (D,)
+    return (U@torch.diag(S))[:, :dims], S[:dims] # (D, dims), (dims,)
